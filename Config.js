d2cfdf7bc039d67584abe496c4e8d829597d3a87
jsdiop <162188851+jsdiop@users.noreply.github.com>
2024-04-04 21:17:15 +0000

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@ const chalk = require('chalk')
 
 //contact details
 global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownername = "Jules"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
